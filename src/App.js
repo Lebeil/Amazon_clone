422d0b29a38cd14d91e5d93457dc6b0bf8266132
Lebeil <liedel75@gmail.com>
2020-11-06 17:13:09 +0100

    add react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import './App.css';
+import React from "react"
+import './App.css'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      Bonjour
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Switch>
+                    <Route path='/checkout'>
+                        <h1>Bonjour Amazon Clone &#128640;</h1>
+                    </Route>
+                    <Route path='/login'>
+                        <h1>Login page</h1>
+                    </Route>
+                    <Route path='/'>
+                        <h1>Home page &#127984;</h1>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
