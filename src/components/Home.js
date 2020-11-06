0d8f070c3e9db1c2391f72b483e869b5c6acc777
Lebeil <liedel75@gmail.com>
2020-11-06 22:13:42 +0100

    add banner + products

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,66 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import Product from "./Product";
 import '../styles/Home.css'
 
 const Home = () => {
     return (
-        <div className="home">
-            <img className="home_image" src="https://images-eu.ssl-images-amazon.com/images/G/08/digital/video/magellan/country/france/Borat2/no_ring/SENG_2020_GWBleedingHero_FT_COVIDUPDATE_XSite_VAR-Daughter_1500X600_PV_fr-FR_176170._CB416710413_.jpg" alt=""/>
-        </div>
+        <Fragment>
+            <div className="home">
+                <img className="home_image"
+                     src="https://images-eu.ssl-images-amazon.com/images/G/08/digital/video/magellan/country/france/Borat2/no_ring/SENG_2020_GWBleedingHero_FT_COVIDUPDATE_XSite_VAR-Daughter_1500X600_PV_fr-FR_176170._CB416710413_.jpg"
+                     alt="amazon"/>
+                {/*Product*/}
+                <div className="home_row">
+                    <Product
+                        id="12321341"
+                        title="Apple MacBook Pro (16 pouces, 16Go RAM, 1To de Stockage, Intel Core i9 2,3GHz) Gris Sidéral"
+                        price={2949.00}
+                        rating={5}
+                        image="https://m.media-amazon.com/images/I/71L2iBSyyOL._AC_UL320_.jpg"
+                    />
+                    <Product
+                        id="12321342"
+                        title="Nouveau Apple iPhone 12 (128 Go) - Bleu"
+                        price={959.00}
+                        rating={4.5}
+                        image="https://m.media-amazon.com/images/I/71ZOtNdaZCL._AC_UL320_.jpg"
+                    />
+                </div>
+                <div className="home_row">
+                    <Product
+                        id="12321341"
+                        title="Nouveau Apple iPhone 12 Pro Max (512 Go) - Graphite"
+                        price={1349.99}
+                        rating={5}
+                        image="https://m.media-amazon.com/images/I/71c7UQ9XPmL._AC_UL320_.jpg"
+                    />
+                    <Product
+                        id="12321541"
+                        title="Dyson V11 Absolute, Bleu, Taille L"
+                        price={829.99}
+                        rating={5}
+                        image="https://m.media-amazon.com/images/I/51T-zO0g+xL._AC_UL320_.jpg"
+                    />
+                    <Product
+                        id="52321341"
+                        title="Véritable® Classic (Blanc) - Potager d’Intérieur Fabriqué en France – Jardin Autonome Intelligent Livré Avec 4 Lingot® - Élu Produit De l’Année 2020"
+                        price={149.09}
+                        rating={5}
+                        image="https://images-eu.ssl-images-amazon.com/images/I/41VY6yrnvBL._AC_SX184_.jpg"
+                    />
+                </div>
+                <div className="home_row">
+                    <Product
+                        id="12321341"
+                        title="Lean Startup: Adoptez l'innovation continue"
+                        price={29.99}
+                        rating={5}
+                        image="https://images-na.ssl-images-amazon.com/images/I/41uNAbThwiL._SX334_BO1,204,203,200_.jpg"
+                    />
+                </div>
+
+            </div>
+        </Fragment>
     );
 };
 
---
+++ src/components/Product.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../styles/Product.css'
+
+function Product ({id, title, image, price, rating}) {
+    return (
+        <div className="product">
+            <div className="product_info">
+                <p>{title}</p>
+                <p className="product_price">
+                    <small>€</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="product_rating">
+                    &#11088;&#11088;&#11088;&#11088;&#11088;
+                    {/*{Array(rating)
+                        .fill()
+                        .map((_) => (
+                            <p>&#11088;</p>
+                        ))}*/}
+                </div>
+            </div>
+
+            <img src={image} alt="lean startup"/>
+            <button>Ajouter au panier</button>
+        </div>
+    );
+};
+
+export default Product;
