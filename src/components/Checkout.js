1dd94616320b0f6a6828c0af38d6c1359d48ac78
Lebeil <liedel75@gmail.com>
2020-11-09 17:56:29 +0100

    firebase deploy

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -5,7 +5,7 @@ import '../styles/Checkout.css'
 import CheckoutProduct from "./CheckoutProduct";
 
 const Checkout = () => {
-    const [{basket}] = useStateValue()
+    const [{basket, user}] = useStateValue()
 
     return (
         <div className="checkout">
@@ -15,22 +15,8 @@ const Checkout = () => {
                      src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668.jpg"
                      alt="ad"/>
                 <div>
+                    <h3>Bonjour, {user?.email}</h3>
                     <h2 className="checkout_title">Shopping basket</h2>
-
-                    {/*<CheckoutProduct
-                        id='112342'
-                        title="This is a test,fijerf ajf ioaj foiajfjaef oiajfij foiejrfo"
-                        image="https://m.media-amazon.com/images/I/51WIKlio9qL.jpg"
-                        price={199.99}
-                        rating={5}
-                    />
-                    <CheckoutProduct
-                        id='112342'
-                        title="This is a test,fijerf ajf ioaj foiajfjaef oiajfij foiejrfo"
-                        image="https://m.media-amazon.com/images/I/51WIKlio9qL.jpg"
-                        price={199.99}
-                        rating={5}
-                    />*/}
 
                     {basket.map((item,i)=> (
                         <CheckoutProduct
@@ -43,10 +29,6 @@ const Checkout = () => {
                         />
 
                     ))}
-                    {/*Basket Product*/}
-                    {/*Basket Product*/}
-                    {/*Basket Product*/}
-                    {/*Basket Product*/}
                 </div>
 
             </div>
