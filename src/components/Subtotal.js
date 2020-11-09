1ab2e5917f49f073aef34a898b9bca315791ab74
Lebeil <liedel75@gmail.com>
2020-11-09 19:33:22 +0100

    add plural method article(s)

--- src/components/Subtotal.js
+++ src/components/Subtotal.js
@@ -3,16 +3,26 @@ import '../styles/Subtotal.css';
 import CurrencyFormat from "react-currency-format"
 import {useStateValue} from "../StateProvider";
 import {getBasketTotal} from "../Reducer";
+import {useHistory} from "react-router-dom";
 
 const Subtotal = () => {
+    const history = useHistory();
     const [{basket}, dispatch] = useStateValue()
+    const formatArticle = basket => {
+        if(basket.length>0){
+            return (`articles`)
+        }else {
+            return (`article`)
+        }
+    }
+
     return (
         <div className="subtotal">
             <CurrencyFormat
                 renderText={(value) => (
                     <>
                         <p>
-                            Total ({basket.length} article(s)): <strong>{value}</strong>
+                            Total ({basket.length} {formatArticle(basket)}): <strong>{value}</strong>
                         </p>
                         <small className="subtotal_gift">
                             <input type="checkbox"/>
@@ -26,7 +36,7 @@ const Subtotal = () => {
             thousandSeparator={true}
             prefix={"€"}
             />
-            <button>Payer</button>
+            <button onClick={e=> history.push('/payment')}>Payer</button>
         </div>
     );
 };
